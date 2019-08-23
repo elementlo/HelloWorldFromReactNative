154e1ba5216b5235de4cb0423d7a268c5fe264da
element <element.zhan@gmail.com>
2019-08-23 15:52:18 +0800

    Build the navigator and register it in App.js

--- app/navigate/AppStack.js
+++ app/navigate/AppStack.js
@@ -1,21 +1,48 @@
 import {createStackNavigator} from 'react-navigation';
 import SplashView from '../view/login/SplashView';
 
-export const StackNavigator = createStackNavigator({
-  Splash: {
-    screen: SplashView,
-    navigationOptions: {
-      header: null,
+export const StackNavigator = createStackNavigator(
+  {
+    Splash: {
+      screen: SplashView,
+      navigationOptions: {
+        header: null,
+      },
+    },
+    Login: {
+      screen: LoginView,
+      navigationOptions: {
+        header: null,
+      },
+    },
+    Register: {
+      screen: RegisterView,
+      navigationOptions: {
+        header: '注册',
+      },
+    },
+    Tab: {
+      screen: TabBottombar,
+      navigationOptions: {
+        header: null,
+      },
     },
   },
-  Login: {
-    screen: LoginView,
+  {
+    initialRouteName: 'Splash',
     navigationOptions: {
-      header: null,
+      headerStyle: {
+        backgroundColor: '#353535',
+        height: 44,
+      },
+      headerTintColor: '#FFFFFF',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        flex: '1',
+        textAlign: 'center',
+        fontSize: 18,
+      },
+      headerRight: <View />
     },
   },
-  Register:{
-      screen:RegisterView,
-      dfdf
-  }
-});
+);
